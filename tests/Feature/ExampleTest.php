6d740af5117675b8f5c7fa4b85fc67ee11011023
Rupesh Singh <rupesh.singhghy@gmail.com>
2024-11-18 09:52:57 +0530

    Update actions/cache to v6

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -12,8 +13,13 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        // Create a test user
+        $user = User::factory()->create();
 
+        // Log the user in
+        $response = $this->actingAs($user)->get('/');
+
+        // Assert that the status code is 200
         $response->assertStatus(200);
     }
 }
